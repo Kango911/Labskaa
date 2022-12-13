77c162993bad2e3c25a7bd32e6d307e0017c7066
Alex <lipenkov.a61@gmail.com>
2022-12-13 21:28:28 +0500

    Fix Menu

--- Labskaa/Menu.cs
+++ Labskaa/Menu.cs
@@ -6,20 +6,27 @@ namespace Labs
     {
         public static void Main()
         {
+            string x;
             Console.WriteLine("Hello! \nWrote number lab...");
-            string x = Console.ReadLine();
-            switch (x)
+            do
             {
-                case "1":
-                    labDM1.LabDM1();
-                    break;
-                case "2":
-                    labDM2.LabDM2();
-                    break;
-                default:
-                    Console.WriteLine("ERROR");
-                    break;
-            }
+                x = Console.ReadLine();
+                switch (x)
+                {
+                    case "1":
+                        labDM1.LabDM1();
+                        break;
+                    case "2":
+                        labDM2.LabDM2();
+                        break;
+                    default:
+                        Console.WriteLine("Введен не верный номер Lab \n Перезапустите программу");
+                        break;
+                }
+            } while (x == "1" | x == "2");
+            
+            
+            
         }
     }
 } 
